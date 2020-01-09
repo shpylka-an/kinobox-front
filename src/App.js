d702cf28eb4226930beed4d9ed470bb16ff28717
Anatolii Shpylka <shpylka.an@gmail.com>
2020-01-09 14:15:29 +0200

    add navigation

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {BrowserRouter as Router} from "react-router-dom";
 
 import Routes from './routes'
+import NavBar from "./components/NavBar";
 
 const App = () => (
     <Router>
+        <NavBar />
         <Routes />
     </Router>
 );
---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import {makeStyles} from '@material-ui/core/styles';
+import {Link, withRouter} from "react-router-dom";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+const NavBar = ({location}) => {
+    const classes = useStyles();
+
+    const NavLink = ({to, title}) => (
+        <MenuItem component={Link} to={to} selected={to === location.pathname}>{title}</MenuItem>
+    );
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon/>
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    News
+                </Typography>
+                <NavLink to='/' title='Home'/>
+                <NavLink to='/login' title='Login'/>
+            </Toolbar>
+        </AppBar>
+    )
+};
+
+export default withRouter(NavBar);
